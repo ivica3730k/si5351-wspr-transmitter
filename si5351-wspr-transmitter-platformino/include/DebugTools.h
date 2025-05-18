2ccc21f0c3684c0db61b06d2e37c04852b4a8958
Ivica <ivica-matic@outlook.com>
2025-05-18 13:30:09 +0100

    added mcp23017 submodule

--- si5351-wspr-transmitter-platformino/include/DebugTools.h
+++ si5351-wspr-transmitter-platformino/include/DebugTools.h
@@ -15,30 +15,40 @@ inline void scan_i2c_bus()
 {
     Wire.begin();
     Serial.println("Starting I2C bus scan...");
-
-    for (uint8_t address = I2C_MIN_ADDRESS; address < I2C_MAX_ADDRESS; ++address)
+    while (true)
     {
-        Wire.beginTransmission(address);
-        uint8_t error = Wire.endTransmission();
+        for (uint8_t address = I2C_MIN_ADDRESS; address < I2C_MAX_ADDRESS; ++address)
+        {
+            Wire.beginTransmission(address);
+            uint8_t error = Wire.endTransmission();
 
-        if (error == 0)
-        {
-            Serial.print("I2C device found at address 0x");
-            if (address < 16)
-                Serial.print("0");
-            Serial.print(address, HEX);
-            Serial.println(" !");
+            if (error == 0)
+            {
+                Serial.print("I2C device found at address 0x");
+                if (address < 16)
+                    Serial.print("0");
+                Serial.print(address, HEX);
+                Serial.println(" !");
+            }
+            else if (error == 4)
+            {
+                Serial.print("Unknown error at address 0x");
+                if (address < 16)
+                    Serial.print("0");
+                Serial.println(address, HEX);
+            }
         }
-        else if (error == 4)
+
+        Serial.println("I2C scan complete.");
+        Serial.println("Waiting for 1 second before next scan...");
+        uint32_t start_time = millis();
+        while (millis() - start_time < SCAN_DELAY_MS)
         {
-            Serial.print("Unknown error at address 0x");
-            if (address < 16)
-                Serial.print("0");
-            Serial.println(address, HEX);
+#ifdef BOARD_FAMILY_ESPRESSIF
+            yield();
+#endif
         }
     }
-
-    Serial.println("I2C scan complete.");
 }
 
 } // namespace debug_tools
