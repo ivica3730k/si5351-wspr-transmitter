a5e031f5784662f8eb59a52c70a8f7e50706d689
Ivica <ivica-matic@outlook.com>
2025-05-26 21:52:15 +0100

    add secrets template

---
+++ si5351-wspr-transmitter-platformino/src/secrets_template.h
@@ -0,0 +1,11 @@
+#ifndef SECRECTS_H
+#define SECRECTS_H
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+#define WSPR_CALLSIGN ""
+#define WSPR_GRIDSQUARE ""
+#define WSPR_DBM 18
+
+#endif // SECRECTS_H
