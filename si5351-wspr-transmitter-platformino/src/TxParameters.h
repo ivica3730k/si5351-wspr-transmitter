6d32f3a6027638c2824d342d5c17d35fab1eb7e3
Ivica <ivica-matic@outlook.com>
2025-07-30 21:43:22 +0100

    feat: add multiband tx

--- si5351-wspr-transmitter-platformino/src/TxParameters.h
+++ si5351-wspr-transmitter-platformino/src/TxParameters.h
@@ -1,14 +1,14 @@
 #ifndef TX_PARAMETERS_H
 #define TX_PARAMETERS_H
 
-#define CALIBRATION_OFFSET -27000
+#define CALIBRATION_OFFSET -91200
 
 #include "TxHardware.h"
-TxHardwareTxParameters tx_params_20m(14095600, CALIBRATION_OFFSET,
+TxHardwareTxParameters tx_params_20m(14095600, -91200,
                                      TxHardwareTxParameters::DriveStrength::MEDIUM_POWER);
 TxHardwareTxParameters tx_params_15m(21095600, CALIBRATION_OFFSET,
                                      TxHardwareTxParameters::DriveStrength::MEDIUM_POWER);
-TxHardwareTxParameters tx_params_10m(28095600, CALIBRATION_OFFSET,
+TxHardwareTxParameters tx_params_10m(28124600, -39250,
                                      TxHardwareTxParameters::DriveStrength::MEDIUM_POWER);
 TxHardwareTxParameters keep_warm_params(100000, CALIBRATION_OFFSET,
                                         TxHardwareTxParameters::DriveStrength::LOW_POWER);
--- si5351-wspr-transmitter-platformino/src/main.cpp
+++ si5351-wspr-transmitter-platformino/src/main.cpp
@@ -6,26 +6,28 @@
 
 #include <Arduino.h>
 
+#ifdef MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
 OnboardFilterHardware filter_hardware(0x20);
 #define DRIVER_AMP_ENABLE_PIN D5
+#endif // MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
 
 #ifdef TIME_USE_ESP8266_WIFI_NTP
 #include <ESP8266WiFi.h>
-#endif
+#endif // TIME_USE_ESP8266_WIFI_NTP
 
 #ifdef TIME_USE_ESP32_WIFI_NTP
 #include <WiFi.h>
-#endif
+#endif // TIME_USE_ESP32_WIFI_NTP
 
 #if defined(TIME_USE_ESP8266_WIFI_NTP) || defined(TIME_USE_ESP32_WIFI_NTP)
 #include "WSPRTxSyncNTP.h"
 const char *wifi_ssid = WIFI_SSID;
 const char *wifi_password = WIFI_PASSWORD;
 WSPRTxSyncNTP tx_sync("uk.pool.ntp.org");
-#elif defined(TIME_USE_DUMMYY)
+#elif defined(TIME_USE_DUMMY)
 #include "WSPRTxSyncDummy.h"
 WSPRTxSyncDummy tx_sync(10000);
-#endif
+#endif // TIME_USE_ESP8266_WIFI_NTP or TIME_USE_ESP32_WIFI_NTP or TIME_USE_DUMMYY
 
 #ifdef TX_HARDWARE_USE_SI5351
 #include "TxHardwareSI5351.h"
@@ -33,13 +35,14 @@ TxHardwareSi5351 tx_hardware(0x60);
 #elif defined(TX_HARDWARE_USE_DUMMY)
 #include "TxHardwareDummy.h"
 TxHardwareDummy tx_hardware(10000);
-#endif
+#endif // TX_HARDWARE_USE_SI5351 or TX_HARDWARE_USE_DUMMY
 
 uint8_t wspr_message[WSPR_SYMBOL_COUNT];
 const char *wspr_callsign = WSPR_CALLSIGN;
 const char *wspr_gridsquare = WSPR_GRIDSQUARE;
 int wspr_dbm = WSPR_DBM;
 
+#ifdef MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
 void disable_driver_amplifier()
 {
     pinMode(DRIVER_AMP_ENABLE_PIN, OUTPUT);
@@ -51,26 +54,30 @@ void enable_driver_amplifier()
     pinMode(DRIVER_AMP_ENABLE_PIN, OUTPUT);
     digitalWrite(DRIVER_AMP_ENABLE_PIN, HIGH);
 }
+#endif // MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
 
 void setup()
 {
+#ifdef MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
+    filter_hardware.begin();
     disable_driver_amplifier();
     filter_hardware.disable_all_filters();
     filter_hardware.disable_sub_megahertz_dump();
+#endif // MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
     Serial.begin(115200);
     delay(2000); // wait for platformino serial monitor to spawn after upload
+    Serial.println("Starting WSPR transmitter...");
     Wire.begin();
 
 #ifdef DEBUG_TOOLS_I2C_SCANNER
     debug_tools::scan_i2c_bus();
-#endif
-    filter_hardware.begin();
+#endif // DEBUG_TOOLS_I2C_SCANNER
 
 #ifdef TEST_SUB_MEGAHERTZ_DUMP
     filter_hardware.enable_sub_megahertz_dump();
     tx_hardware.output_constant_tone(keep_warm_params);
     block_forever();
-#endif
+#endif // TEST_SUB_MEGAHERTZ_DUMP
 
 #ifdef TEST_20M_LOW_PASS_FILTER_WITH_20M_SIGNAL
     enable_driver_amplifier();
@@ -78,7 +85,7 @@ void setup()
     filter_hardware.disable_sub_megahertz_dump();
     tx_hardware.output_constant_tone(tx_params_20m);
     block_forever();
-#endif
+#endif // TEST_20M_LOW_PASS_FILTER_WITH_20M_SIGNAL
 
 #ifdef TEST_15M_LOW_PASS_FILTER_WITH_15M_SIGNAL
     enable_driver_amplifier();
@@ -86,7 +93,7 @@ void setup()
     filter_hardware.disable_sub_megahertz_dump();
     tx_hardware.output_constant_tone(tx_params_15m);
     block_forever();
-#endif
+#endif // TEST_15M_LOW_PASS_FILTER_WITH_15M_SIGNAL
 
 #ifdef TEST_10M_LOW_PASS_FILTER_WITH_10M_SIGNAL
     enable_driver_amplifier();
@@ -94,13 +101,13 @@ void setup()
     filter_hardware.disable_sub_megahertz_dump();
     tx_hardware.output_constant_tone(tx_params_10m);
     block_forever();
-#endif
+#endif // TEST_10M_LOW_PASS_FILTER_WITH_10M_SIGNAL
 
 #if defined(TIME_USE_ESP8266_WIFI_NTP) || defined(TIME_USE_ESP32_WIFI_NTP)
 #ifdef DEBUG_WIFI_CONNECTING
     Serial.print("Connecting to ");
     Serial.println(wifi_ssid);
-#endif
+#endif // TIME_USE_ESP8266_WIFI_NTP or TIME_USE_ESP32_WIFI_NTP
 
     WiFi.mode(WIFI_STA);
     WiFi.begin(wifi_ssid, wifi_password);
@@ -110,15 +117,15 @@ void setup()
         delay(500);
 #ifdef DEBUG_WIFI_CONNECTING
         Serial.print(".");
-#endif
+#endif // TIME_USE_ESP8266_WIFI_NTP or TIME_USE_ESP32_WIFI_NTP
     }
 
 #ifdef DEBUG_WIFI_CONNECTING
     Serial.println("\nWiFi connected");
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
-#endif
-#endif
+#endif // DEBUG_WIFI_CONNECTING
+#endif // TIME_USE_ESP8266_WIFI_NTP or TIME_USE_ESP32_WIFI_NTP
 
     JTEncode encoder;
     encoder.wspr_encode(wspr_callsign, wspr_gridsquare, wspr_dbm, wspr_message);
@@ -127,9 +134,6 @@ void setup()
 void wait_for_next_tx_period()
 {
     uint32_t milliseconds_to_next_tx = tx_sync.get_milliseconds_to_next_tx_period();
-    Serial.print("Milliseconds to next TX: ");
-    Serial.println(milliseconds_to_next_tx);
-
     uint32_t target_time = millis() + milliseconds_to_next_tx;
     while (millis() < target_time)
     {
@@ -139,48 +143,62 @@ void wait_for_next_tx_period()
 
 void transmit_wspr_message_on_20m()
 {
+#ifdef MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
     filter_hardware.disable_all_filters();
     filter_hardware.disable_sub_megahertz_dump();
     filter_hardware.enable_20m_low_pass_filter();
     enable_driver_amplifier();
+#endif // MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
     Serial.println("Transmitting WSPR message on 20m...");
     tx_hardware.transmit_wspr_message(tx_params_20m, wspr_message);
     Serial.println("WSPR message on 20m transmitted");
+#ifdef MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
     disable_driver_amplifier();
     filter_hardware.disable_all_filters();
+#endif // MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
 }
 
 void transmit_wspr_message_on_15m()
 {
+#ifdef MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
     filter_hardware.disable_all_filters();
     filter_hardware.disable_sub_megahertz_dump();
     filter_hardware.enable_15m_low_pass_filter();
     enable_driver_amplifier();
+#endif // MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
     Serial.println("Transmitting WSPR message on 15m...");
     tx_hardware.transmit_wspr_message(tx_params_15m, wspr_message);
     Serial.println("WSPR message on 15m transmitted");
+#ifdef MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
     disable_driver_amplifier();
     filter_hardware.disable_all_filters();
+#endif // MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
 }
 
 void transmit_wspr_message_on_10m()
 {
+#ifdef MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
     filter_hardware.disable_all_filters();
     filter_hardware.disable_sub_megahertz_dump();
     filter_hardware.enable_10m_low_pass_filter();
     enable_driver_amplifier();
+#endif // MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
     Serial.println("Transmitting WSPR message on 10m...");
     tx_hardware.transmit_wspr_message(tx_params_10m, wspr_message);
     Serial.println("WSPR message on 10m transmitted");
+#ifdef MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
     disable_driver_amplifier();
     filter_hardware.disable_all_filters();
+#endif // MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
 }
 
 void keep_warm()
 {
+#ifdef MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
     filter_hardware.disable_all_filters();
     filter_hardware.enable_sub_megahertz_dump();
     tx_hardware.output_constant_tone(keep_warm_params);
+#endif // MULTI_BAND_BOARD_WITH_FILTERS_AND_AMPLIFIER
 }
 
 void loop()
@@ -189,4 +207,8 @@ void loop()
     Serial.println("Waiting for next TX period...");
     wait_for_next_tx_period();
     transmit_wspr_message_on_20m();
+    keep_warm();
+    Serial.println("Waiting for next TX period...");
+    wait_for_next_tx_period();
+    transmit_wspr_message_on_10m();
 }
